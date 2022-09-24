50a4aa7e689d8ff42210963550d39dc362c06e71
MurituMaina <vincent.maina@student.moringaschool.com>
2022-09-24 12:35:31 +0300

    add initial files

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AccountContainer from "./AccountContainer";
+
+function App() {
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment violet inverted">
+        <h2>The Royal Bank of Flatiron</h2>
+      </div>
+      <AccountContainer />
+      <p>Hellow World!!</p>
+      <p>Hellow World!!</p>
+      <p>Hellow World!!</p>
+    </div>
+  );
+}
+
+export default App;
